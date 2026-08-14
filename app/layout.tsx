67e0fe71339de2788c008ebcad92d835f7639b6f
LAKSHMMAN HAMAL <ERYXX.03@GMAIL.COM>
2026-08-14 23:07:24 +0545

    add adsense meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-9961296446712353"
+        />
+      </head>
+
       <body className="min-h-full flex flex-col">
         <Script
           async
