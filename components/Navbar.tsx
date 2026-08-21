abc678582e4c97297db97aaa4655a1be4502b60c
LAKSHMMAN HAMAL <ERYXX.03@GMAIL.COM>
2026-08-21 13:38:57 +0545

    Add Sign Up button to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,8 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 z-50 w-full border-b border-white/10 bg-black/70 px-6 py-4 backdrop-blur-md">
       <div className="mx-auto flex max-w-7xl items-center justify-between">
+        
+        {/* Logo */}
         <Link
           href="/"
           className="text-2xl font-bold text-white"
@@ -13,6 +15,7 @@ export default function Navbar() {
           Cinema<span className="text-red-500">Hunt</span>
         </Link>
 
+        {/* Navigation */}
         <div className="flex items-center gap-6 text-sm text-gray-300">
           <Link
             href="/"
@@ -33,6 +36,14 @@ export default function Navbar() {
             className="transition hover:text-white"
           >
             Popular
+          </Link>
+
+          {/* Sign Up */}
+          <Link
+            href="/signup"
+            className="rounded-full bg-red-600 px-5 py-2 font-semibold text-white transition hover:bg-red-700"
+          >
+            Sign Up
           </Link>
         </div>
       </div>
