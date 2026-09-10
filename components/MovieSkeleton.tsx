a921e67c7002082fb5b663917062fa124fcd4780
LAKSHMMAN HAMAL <ERYXX.03@GMAIL.COM>
2026-09-10 14:02:37 +0545

    Fix movie skeleton

--- components/MovieSkeleton.tsx
+++ components/MovieSkeleton.tsx
@@ -1,37 +1,23 @@
-```tsx
 export default function MovieSkeleton() {
   return (
     <div className="flex h-full min-w-0 flex-col overflow-hidden rounded-xl border border-white/10 bg-zinc-900/80">
       {/* Poster skeleton */}
-      <div className="relative aspect-[2/3] w-full overflow-hidden bg-zinc-800">
-        <div className="absolute inset-0 -translate-x-full animate-[shimmer_1.8s_infinite] bg-gradient-to-r from-transparent via-white/10 to-transparent" />
-      </div>
+      <div className="aspect-[2/3] w-full animate-pulse bg-zinc-800" />
 
       {/* Content skeleton */}
       <div className="flex flex-1 flex-col p-3">
-        {/* Title - matches 2 line movie title */}
+        {/* Title */}
         <div className="space-y-2">
-          <div className="relative h-4 w-full overflow-hidden rounded bg-zinc-800">
-            <div className="absolute inset-0 -translate-x-full animate-[shimmer_1.8s_infinite] bg-gradient-to-r from-transparent via-white/10 to-transparent" />
-          </div>
-
-          <div className="relative h-4 w-3/5 overflow-hidden rounded bg-zinc-800">
-            <div className="absolute inset-0 -translate-x-full animate-[shimmer_1.8s_infinite] bg-gradient-to-r from-transparent via-white/10 to-transparent" />
-          </div>
+          <div className="h-4 w-full animate-pulse rounded bg-zinc-800" />
+          <div className="h-4 w-3/5 animate-pulse rounded bg-zinc-800" />
         </div>
 
         {/* Year + rating */}
         <div className="mt-3 flex items-center justify-between">
-          <div className="relative h-3 w-12 overflow-hidden rounded bg-zinc-800">
-            <div className="absolute inset-0 -translate-x-full animate-[shimmer_1.8s_infinite] bg-gradient-to-r from-transparent via-white/10 to-transparent" />
-          </div>
-
-          <div className="relative h-3 w-10 overflow-hidden rounded bg-zinc-800">
-            <div className="absolute inset-0 -translate-x-full animate-[shimmer_1.8s_infinite] bg-gradient-to-r from-transparent via-white/10 to-transparent" />
-          </div>
+          <div className="h-3 w-12 animate-pulse rounded bg-zinc-800" />
+          <div className="h-3 w-10 animate-pulse rounded bg-zinc-800" />
         </div>
       </div>
     </div>
   );
 }
-```
